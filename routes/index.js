cffcecbb52ff98ee723822bf0108aa86ac174ebc
Bill Shannon <bill@billshannon.com>
2015-11-10 12:10:05 -0700

    add json data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/api/v1/guitars', function (req, res, next) {
+    res.json([
+        {id: 1, make : 'Martin', model: 'D15'},
+        {id: 2, make : 'Fender', model: 'Stratocaster'},
+        {id: 3, make : 'Gibson', model: 'Les Paul'}
+    ])
 });
+
+/* GET home page. */
+router.get('*', function(req, res, next) {
+    res.sendFile('index.html', {
+        root : __dirname + '/../public/'
+    })
+});
+
+
 
 module.exports = router;
